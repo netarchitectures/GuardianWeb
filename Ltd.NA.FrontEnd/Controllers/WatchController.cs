1d4d2d695206d8103100fc1e6b297f43a0c38a07
info@netarchitectures.co.uk <info@netarchitectures.co.uk>
2015-07-06 17:04:45 +0200

    Commit before changing carousel

---
+++ Ltd.NA.FrontEnd/Controllers/WatchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ltd.NA.FrontEnd.Controllers
+{
+    public class WatchController : Controller
+    {
+        // GET: Watch
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Round()
+        {
+            return View();
+        }
+
+        public ActionResult Square()
+        {
+            return View();
+        }
+
+        public ActionResult ComingSoon()
+        {
+            return View();
+        }
+    }
+}
